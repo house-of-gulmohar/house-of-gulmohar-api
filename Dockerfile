0015d288bfabd9d5ced10ce70f5790ed3f757f65
anandu.reghu <anandu.reghu@qburst.com>
2022-12-11 20:59:09 +0530

    db initial set up

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,14 @@ FROM node:16
 
 WORKDIR /app
 
-COPY 
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+RUN npm run build
+
+EXPOSE 3000
+
+CMD [ "node", "dist/src/main.js"]
--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,18 +1,25 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger, Type } from '@nestjs/common';
 import { Pool } from 'pg';
+import { from, Observable } from 'rxjs';
 import { CONNECTION_POOL } from 'src/utils/constants';
 
 @Injectable()
 export class DatabaseService {
+  logger = new Logger(DatabaseService.name);
   constructor(@Inject(CONNECTION_POOL) private pool: Pool) {
     this.pool
       .connect()
       .then(() => {
-        console.log('connection success');
+        this.logger.log('connected to postgres');
       })
       .catch((err) => {
-        console.log(err);
-        console.log('connection failed');
+        this.logger.error('db connection failed');
+        this.logger.error(err);
       });
+  }
+
+  private runQuery(query: string, params: any[]): Observable<any> {
+    const now = Date.now();
+    return from(this.pool.query(query, params)).pipe();
   }
 }
