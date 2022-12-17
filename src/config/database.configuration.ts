64b52dc95990c29e74873873651ce5c4c4eaad0d
anandu.reghu <anandu.reghu@qburst.com>
2022-12-17 20:48:16 +0530

    removed nest js files

--- src/config/database.configuration.ts
+++
@@ -1,9 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export default registerAs('database', () => ({
-  host: process.env.DATABSE_HOST || 'localhost',
-  port: process.env.DATABASE_PORT || 5432,
-  db: process.env.DATABASE_DB,
-  user: process.env.DATABASE_USER || 'postgres',
-  password: process.env.DATABASE_PASSWORD,
-}));
